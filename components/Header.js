335178b17679b804652f78899b6c750a73d2dfdc
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-03-18 14:54:06 +0530

    fixes login responsive

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@
 
 const NavBar = () => {
     return (
-        <div>
+        <div className="lg:hidden w-100vw">
         <nav className="bg-black h-16 p-4">
             <h1 className="text-white text-xl font-bold ">Board.</h1>
         </nav>
