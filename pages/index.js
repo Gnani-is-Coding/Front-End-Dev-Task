fb8cf744b7035c694d532732997e16bd14a57f58
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-03-21 14:10:04 +0530

    adds, authorization to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import Image from "next/image";
 import Dashboard from "./dashboard";
+import { useSession } from "next-auth/react";
+import { getSession } from "next-auth/react"; 
 
 
 export default function Home() {
-  return (
-      <div>
+  const {data: session, status} = useSession()
+
+  if (status === 'authenticated') {
+    return(
+    <div>
         <Dashboard/>
       </div>
-  );
+    )
+    
+  }
+  
+}
+
+//For Service Side Rendering
+export const  getServerSideProps =  async(context) => {
+  const session = await getSession(context)
+
+  if (!session) {
+      return{
+          redirect:{
+              destination: '/login',
+          }
+      }
+  }
+
+  return {
+      props: {session},
+  }
 }
