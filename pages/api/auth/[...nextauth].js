bad0c0b2f093198e592ed014f00b99e2b8bd38df
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-06-10 13:35:11 +0530

    adds next secret env var

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,7 +15,7 @@ export const authOptions = {
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     })
-
+    secret: process.env.NEXTAUTH_SECRET,
   ],
 }
 
